dae671dd1081771d4ae396f3a51410875f15f81b
aditya491929 <aditya.malwade@somaiya.edu>
2021-02-19 17:39:48 +0530

    Major update VFL 1.0

--- VFL/VFL/urls.py
+++ VFL/VFL/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+# from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls'))
+    path('',include('main.urls')),
+    
 ]
--- VFL/main/models.py
+++ VFL/main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True, null=False)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=False)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+	    return self.name
+
+# class Product(models.Model):
+# 	CATEGORY = (
+# 			('Indoor', 'Indoor'),
+# 			('Out Door', 'Out Door'),
+# 			) 
+
+# 	name = models.CharField(max_length=200, null=True)
+# 	price = models.FloatField(null=True)
+# 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+# 	description = models.CharField(max_length=200, null=True, blank=True)
+# 	date_created = models.DateTimeField(auto_now_add=True, null=True)
+# 	tags = models.ManyToManyField(Tag)
+
+# 	def __str__(self):
+# 		return self.name
+
+class Post(models.Model):
+    note = models.CharField(max_length=1000, null=True)
+    photo = models.ImageField()
+    user = models.ForeignKey(User, null=True, on_delete= models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    category = models.CharField(max_length=200, null=True)
+   
+
+    def __str__(self):
+	    return self.name
