996e94625e8725fcd7c357554db6bc09493f4cc1
RahulPanchal-15 <rahulkkm151@gmail.com>
2021-02-28 12:53:20 +0530

    First Commit on Temp main

--- VFL/VFL/settings.py
+++ VFL/VFL/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'crispy_forms',
     'main.apps.MainConfig',
 ]
 
@@ -129,3 +129,4 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+CSRIPY_TEMPLATE_PACK = "bootstrap4"
