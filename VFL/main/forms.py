d60d5ebdd30d991b9d1dc4d836161f56ffa31bb8
aayushOz11 <aayushmalde1919@gmail.com>
2021-03-21 17:44:13 +0530

    added dropdown fiels

--- VFL/main/forms.py
+++ VFL/main/forms.py
@@ -4,6 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+state_choices = (("Andhra Pradesh","Andhra Pradesh"),("Arunachal Pradesh ","Arunachal Pradesh "),("Assam","Assam"),("Bihar","Bihar"),
+("Chhattisgarh","Chhattisgarh"),("Goa","Goa"),("Gujarat","Gujarat"),("Haryana","Haryana"),("Himachal Pradesh","Himachal Pradesh"),
+("Jammu and Kashmir ","Jammu and Kashmir "),("Jharkhand","Jharkhand"),("Karnataka","Karnataka"),("Kerala","Kerala"),("Madhya Pradesh","Madhya Pradesh"),("Maharashtra","Maharashtra"),("Manipur","Manipur"),("Meghalaya","Meghalaya"),("Mizoram","Mizoram"),("Nagaland","Nagaland"),("Odisha","Odisha"),("Punjab","Punjab"),("Rajasthan","Rajasthan"),("Sikkim","Sikkim"),("Tamil Nadu","Tamil Nadu"),("Telangana","Telangana"),("Tripura","Tripura"),("Uttar Pradesh","Uttar Pradesh"),("Uttarakhand","Uttarakhand"),("West Bengal","West Bengal"),("Andaman and Nicobar Islands","Andaman and Nicobar Islands"),("Chandigarh","Chandigarh"),("Dadra and Nagar Haveli","Dadra and Nagar Haveli"),("Daman and Diu","Daman and Diu"),("Lakshadweep","Lakshadweep"),("National Capital Territory of Delhi","National Capital Territory of Delhi"),("Puducherry","Puducherry"))
+
+shipping=(("Local","Local"),("Global","Global"),("Span-India","Span-India"))
 class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
@@ -14,7 +19,7 @@ class CreatePost(forms.Form):
 	category = forms.CharField(label="Category")
 	description = forms.CharField(label="Description",widget=forms.Textarea(attrs={"rows":2,"cols":40}))
 	image = forms.FileField()
-	availability = forms.CharField(label="Shipping..." )
+	availability = forms.CharField(label="Shipping...",widget=forms.Select(choices=shipping) )
 
 
 class ProfileForm(forms.Form):
@@ -22,7 +27,7 @@ class ProfileForm(forms.Form):
 	phone = forms.CharField(label = 'Phone' ,required = True,max_length=10,widget=forms.TextInput(attrs={'placeholder':''}))
 	email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':''}))
 	business_name = forms.CharField(label='Business Name',max_length=100,widget=forms.TextInput(attrs={'placeholder':' '}))
-	state = forms.CharField(label="State", max_length=20,widget=forms.TextInput(attrs={'placeholder':''}))
+	state = forms.CharField(label="State",widget=forms.Select(choices=state_choices))
 	city = forms.CharField(label='City' , max_length=20,widget=forms.TextInput(attrs={'placeholder':''}))
 	postal_code = forms.CharField(label="Postal Code", max_length=6,widget=forms.TextInput(attrs={'placeholder':''}))
 	address = forms.CharField(label="Address", max_length=100,widget=forms.TextInput(attrs={'placeholder':''}))
